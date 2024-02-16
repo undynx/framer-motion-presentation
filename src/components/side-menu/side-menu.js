b5a15e90ecbe122e4850aea68a4907f6f20a8c6a
Maite <martinezf.maite@gmail.com>
2024-02-16 09:33:12 -0300

    Animate side menut

--- src/components/side-menu/side-menu.js
+++ src/components/side-menu/side-menu.js
@@ -1,27 +1,69 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+
 import { ReactComponent as MenuSVG } from '../../assets/icons/menu.svg';
 import './side-menu.css';
 
 const links = ["One", "Two", "Three", "Four"]
 
+const variants = {
+  opened: { x: 0 },
+  closed: { x: '-100%', transition: { delay: 1 } }
+}
+
+const ulVariants = {
+  opened: {
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.2,
+    }
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.2,
+      staggerDirection: -1
+    }
+  }
+}
+
+const liVariants = {
+  opened: {
+    y: 0,
+    opacity: 1
+  },
+  closed: {
+    y: -20,
+    opacity: 0
+  }
+}
+
 const SideMenu = ({ isOpen, onClose }) => {
   return (
-    <>
-      {isOpen && (
-        <div className="menuContainer">
-          <button className="menuButton sideMenuButton" onClick={onClose}>
-              <MenuSVG className="menuIcon" />
-          </button>
-          <ul className="menuInside">
-            {links.map((link) => (
-              <li className="li">
-                <a href="#" className="link" key={link}>{link}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
-    </>
+    <motion.div
+      className="menuContainer"
+      variants={variants}
+      animate={ isOpen ? "opened" : "closed" }
+      transition={{ duration: 0.3 }}
+    >
+      <button className="menuButton sideMenuButton" onClick={onClose}>
+          <MenuSVG className="menuIcon" />
+      </button>
+      <motion.ul
+        variants={ulVariants}
+        className="menuInside"
+        animate={ isOpen ? "opened" : "closed" }
+      >
+        {links.map((link) => (
+          <motion.li
+            variants={liVariants}
+            className="li"
+            animate={ isOpen ? "opened" : "closed" }
+          >
+            <a href="#" className="link" key={link}>{link}</a>
+          </motion.li>
+        ))}
+      </motion.ul>
+    </motion.div>
   )
 }
 
